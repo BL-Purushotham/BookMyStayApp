45b6f59d2bb9d163a69bba93bf5c09eb8fb2578e
BL-Purushotham <purushotham.cirimala@bridgelabz.com>
2026-06-19 12:14:23 +0530

    UC4-ReservationConformation

---
+++ src/com/bridgelabz/bookmystayapp/BookMyStayApp.java
@@ -0,0 +1,86 @@
+package com.bridgelabz.bookmystayapp;
+
+public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        InventoryService inventory = new InventoryService();
+
+        inventory.addRoomType("Single", 10, 2000);
+        inventory.addRoomType("Double", 5, 3500);
+        inventory.addRoomType("Suite", 2, 7000);
+
+        System.out.println("===== HOTEL INVENTORY =====");
+
+        inventory.displayInventory();
+
+        System.out.println("===== SEARCHING ROOMS =====");
+
+        SearchService searchService =
+                new SearchService(inventory);
+
+        System.out.println("Searching Single Room");
+
+        searchService.searchRoom("Single");
+
+        System.out.println();
+
+        System.out.println("Searching Double Room");
+
+        searchService.searchRoom("Double");
+
+        System.out.println("BOOKING QUEUE SERVICE");
+        BookingQueueService queue = new BookingQueueService();
+
+        Reservation r1 =
+                new Reservation(
+                        "R101",
+                        "Lakshmi",
+                        "Single");
+
+        Reservation r2 =
+                new Reservation(
+                        "R102",
+                        "Kumar",
+                        "Double");
+        Reservation r3 =
+                new Reservation(
+                        "R103",
+                        "Priya",
+                        "Suite");
+
+        queue.addBookingRequest(r1);
+        queue.addBookingRequest(r2);
+        queue.addBookingRequest(r3);
+
+        System.out.println("\nProcessing Queue");
+
+        while(queue.hasPendingRequests()) {
+
+            Reservation reservation =
+                    queue.getNextRequest();
+
+            System.out.println(
+                    reservation.getReservationId()
+                            + " processed");
+
+        }
+        System.out.println("==============BOOKING CONFIRMATION==========================");
+        BookingService bookingService =
+                new BookingService(inventory);
+
+        while(queue.hasPendingRequests()) {
+
+            Reservation reservation =
+                    queue.getNextRequest();
+
+            bookingService.confirmBooking(
+                    reservation);
+        }
+
+        System.out.println(
+                "\nRemaining Rooms : "
+                        + inventory.getAvailableRooms(
+                        "Single"));
+    }
+
+}
